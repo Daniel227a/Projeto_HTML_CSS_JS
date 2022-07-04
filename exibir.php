7c5de1f2a9d9f83714047d9c2db77126e32e3373
Daniel_Pereira_Monteiro <49257791+Daniel227a@users.noreply.github.com>
2022-07-04 05:58:06 -0300

    div feita

--- exibir.php
+++ exibir.php
@@ -8,9 +8,79 @@
     <title>novo evento</title>
     <script src="js/scripts.js" defer></script>
     <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
+    <style>
+    .titulo {
+        line-height: 80px;
+        text-align-last: center;
+
+        background-color: black;
+        color: rgb(238, 255, 4);
+    }
+
+
+    .menu {
+        width: 25%;
+
+        border-style: none;
+        height: 755px;
+        border: 11px;
+        border-color: rgba(219, 53, 53, 0.5), ;
+
+
+
+
+
+
+        background-color: rgb(255, 255, 255);
+        float: left;
+        background-image: linear-gradient(rgba(255, 255, 255, 0.5), rgba(2, 0, 0, 0.959))
+    }
+
+    .link {
+        text-align: center;
+        width: 20%;
+        font-size: 22px;
+
+        background-color: rgb(255, 31, 31);
+        color: black;
+    }
+
+    .caixa {
+
+        color: rgb(0, 0, 0);
+        background: rgba(212, 162, 162, 0.575);
+        text-align: justify;
+        width: 35%;
+
+        border-top-right-radius: 30px;
+        border-bottom-left-radius: 30px;
+        border: 2px red inset;
+        ;
+        display: inline-block;
+
+
+    }
+
+    footer {
+        position: absolute;
+        bottom: 0;
+
+        color: black;
+        width: 100%;
+        font-size: 32px;
+
+        text-align: center;
+
+
+    }
+
+    #grad {
+        background-image: linear-gradient(rgba(255, 255, 255, 0.5), rgba(2, 0, 0, 0.959));
+    }
+    </style>
 </head>
 
-<body>
+<body id="grad">
     <?php
 
             include("config.php");
@@ -24,24 +94,46 @@
 
             while($row_nome = mysqli_fetch_assoc($res)){
                 echo"<br>";
-                echo"<br> nome: ".$row_nome["nome"];
-                echo"<br> nome: ".$row_nome["descriçao"];
-                echo"<br> nome: ".$row_nome["inicio"];
+              //  echo"<br> nome: ".$row_nome["nome"];
+               // echo"<br> Descriçao ".$row_nome["descriçao"];
+               // echo"<br> inicio: ".$row_nome["inicio"];
                 $id_img=$row_nome["id_img"];
                 $id_tipo=$row_nome["id_tipo"];
                 $sql1="SELECT path FROM arquivos WHERE id='$id_img'";
                 $res1=$conn->query($sql1);
                 while($row_img = mysqli_fetch_assoc($res1)){
                     $temp=$row_img["path"];
-                    echo"<p>arquivo enviado</p>,<a href=\"$temp\"> mostrar imagem</a></p>";
-                    echo '<img  width="18%" height="20%" src="'.$row_img['path'].'">';
-                
+                   // echo"<p>arquivo enviado</p>,<a href=\"$temp\"> mostrar imagem</a></p>";
+                    //echo"<br>";
+                    echo 
+                    '<div>
+                   
+                        <div class="caixa">
+                       
+                            <center>
+                                nome: "'.$row_nome["nome"].'"
+                                <br>
+                                Descriçao: "'.$row_nome["descriçao"].'"
+                                <br>
+                                inicio: "'.$row_nome["inicio"].'"
+                              
+                                <img  width="58%" height="20%" src="'.$row_img['path'].'" >
+                            </center>
+                        </div>
+                     
+                    </div>
+                  ';
+                  
                 
                 }
             }
 
         ?>
 
+    <div>
+
+
+    </div>
 </body>
 
 </html>
